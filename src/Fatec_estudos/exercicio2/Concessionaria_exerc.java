bb10594c29823ff811a1e4dd1f1fbcd08a5f4f71
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-07-22 16:19:01 -0300

    Exercício 2 feito

---
+++ src/Fatec_estudos/exercicio2/Concessionaria_exerc.java
@@ -0,0 +1,21 @@
+
+import javax.swing.JOptionPane;	
+public class Concessionaria_exerc
+{
+	public static void main(String[] args)
+	{
+		Automovel_exerc auto = new Automovel_exerc();
+
+		auto.setModelo(JOptionPane.showInputDialog("Digite o modelo:"));
+		auto.setCor(JOptionPane.showInputDialog("Digite a cor:"));
+		auto.setMarca(JOptionPane.showInputDialog("Digite a marca:"));
+		auto.setPreco(Double.parseDouble(JOptionPane.showInputDialog("Digite o preço:")));
+		/*metodo set   metodo de conversão              recebendo o valor
+		Forma separada:
+		x=JOptionPane.showInputDialog("Digite o preço:");
+		y=Double.parseDouble(x);
+		auto.setPreco(y);*/
+		auto.Informa_Automovel();
+
+	}
+}
