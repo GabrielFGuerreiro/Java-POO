a226a714b8c7ef8727c4db20aaf463efe2dbd74f
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-07-25 13:18:31 -0300

    Exercicio 11 feito

---
+++ src/Fatec_estudos/exercicio11/Quarto_ciclo_ADS.java
@@ -0,0 +1,21 @@
+package exemplo11;
+
+public class Quarto_ciclo_ADS extends Secretaria_escolar
+{
+    public static void main(String[] args)
+    {
+        //Chama o mét. const. padrão
+        Secretaria_escolar  A = new Secretaria_escolar();
+        Insere_Dados(A); //É possível passar a variável de instância como parâmetro
+
+        //Chama o mét. const. padrão, passando parâmetros
+        Secretaria_escolar B = new Secretaria_escolar("Maria",22,7.7,"ADS");
+        Insere_Dados(B); //É possível passar a variável de instância como parâmetro
+    }
+
+    public static void Insere_Dados(Secretaria_escolar C) //Recebendo o valor da variável de instância por parâmetro
+    {
+        System.out.println("Nome do aluno: \t" + C.getNome() + "\nIdade do aluno: \t" + C.getIdade() +
+        "\nMédia do aluno: \t" + C.getMedia() + "\nCurso do aluno: \t" + C.getCurso());            
+    }
+}
