9fdef0c1ffb0dd8a403e2c2e81cf1c7c82bcdc0e
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-07-25 13:27:20 -0300

    Exercicio 12 feito

---
+++ src/Fatec_estudos/exercicio12/Cine_Caicara.java
@@ -0,0 +1,6 @@
+package exemplo12;
+
+public class Cine_Caicara extends Cinemas_antigos
+{
+    Cine_Caicara(String antigo) {super(antigo);}
+}
---
+++ src/Fatec_estudos/exercicio12/Cinemas_antigos.java
@@ -0,0 +1,13 @@
+package exemplo12;
+
+public class Cinemas_antigos extends Cinemas_Santos
+{
+    private String nome;
+
+    Cinemas_antigos(String antigo)
+    {
+        System.out.println("História dos cinemas de Santos\n" + antigo);        
+    }
+    public void setNome(String nome) {this.nome = nome;}
+    public String getNome() {return nome;}
+}
