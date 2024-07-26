ae5e9fc2c192a662bf2b6743f3a051dce6d3c6d3
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-07-26 13:22:03 -0300

    Exercicio 15 feito

---
+++ src/Fatec_estudos/exercicio15/ADS.java
@@ -0,0 +1,27 @@
+package exemplo15;
+
+public class ADS extends Fatec
+{
+    Fatec curso1 = new Fatec();
+
+    public ADS()
+    {
+        curso1.mostracurso();
+
+        curso1.setNomecurso("Tecnologia em Análise e Desenvolvimento de Sistemas");
+        curso1.setQNTDalunos((100));
+
+        curso1.attalunos(50);
+        curso1.mostracurso();
+
+        mostracurso();
+    }
+
+    public void mostracurso()
+    {
+        curso1.setNomecurso("Bacharel em Análise e Desenvolvimento de Sistemas");
+        curso1.setQNTDalunos((40));
+        System.out.println("Nome do curso: " + curso1.getNomecurso() +
+        "\nQuantidade de alunos: " + curso1.getQNTDalunos());
+    }
+}
---
+++ src/Fatec_estudos/exercicio15/Fatec_BS.java
@@ -0,0 +1,9 @@
+package exemplo15;
+
+public class Fatec_BS
+{
+    public static void main(String[] args)
+    {
+        ADS A = new ADS();        
+    }    
+}
