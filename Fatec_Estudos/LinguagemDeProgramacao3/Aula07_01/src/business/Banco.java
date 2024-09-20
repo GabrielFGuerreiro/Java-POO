c24d50c57293a00e302746b22af1a786df7f0d0c
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-09-20 15:56:59 -0300

    Aula 07_01 - Classe banco getters e setters

--- Fatec_Estudos/LinguagemDeProgramacao3/Aula07_01/src/business/Banco.java
+++ Fatec_Estudos/LinguagemDeProgramacao3/Aula07_01/src/business/Banco.java
@@ -2,5 +2,17 @@ package business;
 
 public class Banco
 {
-    private int listaDeClientes, listaDeContas;
+    private int listaDeClientes = 0, listaDeContas = 0;
+
+    public Banco(int listaDeClientes, int listaDeContas)
+    {
+        this.listaDeClientes = listaDeClientes;
+        this.listaDeContas = listaDeContas;
+    }
+
+    public int getListaClientes(){return listaDeClientes;}
+    public int getListaDeContas() {return listaDeContas;}
+
+    public void setListaDeClientes(int listaDeClientes) {this.listaDeClientes = listaDeClientes;}
+    public void setListaDeContas(int listaDeContas) {this.listaDeContas = listaDeContas;}
 }
