c899dd83899777e6b1c23c57786e6f8238235262
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-09-27 15:01:37 -0300

    Menu - Criada opção para listar autores

--- Fatec_Estudos/LinguagemDeProgramacao3/Aula07_02/src/business/Autor.java
+++ Fatec_Estudos/LinguagemDeProgramacao3/Aula07_02/src/business/Autor.java
@@ -1,5 +1,7 @@
 package business;
 
+import java.util.ArrayList;
+
 public class Autor
 {
     private String nome, nacionalidade, dataDeNascimento;
@@ -13,4 +15,19 @@ public class Autor
     public String getNome() {return nome;}
     public String getNacionalidade() {return nacionalidade;}
     public String getDataDeNascimento() {return dataDeNascimento;}
+
+    public static void  listaAutores(ArrayList<Autor> autores) 
+    {//Método é static para não ter que criar um objeto (na classe principal) que iria apenas ser usado para acessar esse método.
+        if(!autores.isEmpty())
+        {   
+            System.out.println("           \n[Lista de Autores]");
+            for (Autor autor : autores)  //Exibe a lista de autores existentes com um for-each
+            {
+                System.out.println("\nNome: " + autor.getNome() + "\nNascionalidade: " + autor.getNacionalidade() +
+                "\nData de nascimento: " + autor.getDataDeNascimento()); 
+            }
+        }
+        else
+            System.out.println("Lista de autores vazia! Cadastre pelo menos um autor");
+    }
 }
--- Fatec_Estudos/LinguagemDeProgramacao3/Aula07_02/src/view/Principal.java
+++ Fatec_Estudos/LinguagemDeProgramacao3/Aula07_02/src/view/Principal.java
@@ -3,39 +3,48 @@ package view;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import business.Livro;
 import business.Autor;
 import business.Cadastros;
-import business.Livro;
 
 public class Principal
 {
     public static void main(String[] args)
     {       
-        Scanner scanner = new Scanner(System.in); //Cria o scanner para receber input do user
+        ArrayList<Livro> livros = new ArrayList<>(); //Cria a lista de livros
+        ArrayList<Autor> autores = new ArrayList<>(); //Cria a lista de autores
         Cadastros cadastrar = new Cadastros(); //Instância do cadastro
+        Scanner scanner = new Scanner(System.in); //Cria o scanner para receber input do user
 
         while(true)
         {
-            System.out.println("       Menu\n1.Cadastrar livro\n2.Cadastrar autor\n0.Sair");
+            System.out.println("       Menu\n1.Cadastrar livro\n2.Cadastrar autor\n3.Lista livros\n4.Lista autores\n0.Sair");
             int numero = scanner.nextInt(); scanner.nextLine(); //Pega o enter
 
-            switch (numero)
+            switch(numero)
             {
                 case 1:
-                    cadastrar.cadastrarLivro(scanner); //Chama o método de cadastro, enviando como parâmetros o scanner e o autor
+                    cadastrar.cadastrarLivro(scanner, livros, autores); //Chama o método de cadastro, enviando como parâmetros o scanner, e as listas de livros e de autores
                     System.out.println("Livro cadastrado com sucesso!\n");
   
                     break;
             
                 case 2:
-                    cadastrar.cadastrarAutor(scanner);
+                    cadastrar.cadastrarAutor(scanner, autores); //Chama o método de cadastro, enviando como parâmetros o scanner e a lista de autores
                     System.out.println("Autor cadastrado com sucesso!\n");
 
                     break;
+                
+                case 4:
+                    //Autor objAutor = new Autor(null, null, null);  <--- outra possibilidade era criar uma instância, enviando parâmetros nulos.
+                    //       ^--- Não muito recomendada, pois criaria um objeto apenas para acessar um método.
+                    Autor.listaAutores(autores);  //Um método static pode ser chamado diretamente pelo nome da classe, sem precisar criar um objeto.
+             
+                    break;
 
                 case 0:
-                    scanner.close(); // Fecha o Scanner
-                    return; // Encerra o programa
+                    scanner.close(); //Fecha o Scanner
+                    return; //Encerra o programa (sai do while infinito)
 
                 default:
                     System.out.println("Opção inválida, tente novamente.");
@@ -43,5 +52,4 @@ public class Principal
             }//switch
         }//while
     }//main
-
 }//classe
